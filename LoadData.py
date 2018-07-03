0bce8f520d8716963a80039adc1acfe6ce4dd512
tfmrodge <38299576+tfmrodge@users.noreply.github.com>
2018-07-03 17:39:16 -0400

    BCBlues_Class
    Created the BC Blues class, worked on the steady state version for syntax/python learning

--- LoadData.py
+++ LoadData.py
@@ -9,7 +9,7 @@ Created on Fri Jun 15 14:51:15 2018
 import numpy as np
 import pandas as pd
 
-
+#cd "D:\Users\Tim Rodgers\Documents\GitHub\BioretentionBlues"
 chemsumm = pd.read_csv('OPECHEMSUMM.csv') 
 #For the BCsumm, dimensions are the dimensions of the BC. Since the ponding zone is transient, its depth, area etc. will change.
-bcsumm = pd.read_csv('BCSUMM.csv', index_col = 'Compartment') 
+bcsumm = pd.read_csv('BCSUMM.csv') 
---
+++ Practice.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 19 14:14:25 2018
+
+@author: Tim Rodgers
+"""
+
+
+x = BioretentionCellBlues(bcsumm,chemsumm)
+y = x.steady_state(bcsumm,chemsumm)
