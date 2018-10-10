6d65e6547f73f1a5d362eb561ae9f4c516286abd
tfmrodge <tim.rodgers@utoronto.ca>
2018-10-10 16:37:22 -0700

    Loop
    This one still has the loop in case I ever need the code

--- ADRE_Practics.py
+++ ADRE_Practics.py
@@ -15,6 +15,8 @@ for all inputs (eg Z, D values) to be non-uniform spatially.
 import numpy as np
 import pandas as pd
 import time 
+from scipy.interpolate import interp1d
+from matplotlib import pyplot as plt
 
 start = time.time()
 #Testing, so lets set up a simple problem. 
@@ -26,6 +28,8 @@ dx = 1 #Space step [l]
 #dt = 1 #time step [t]
 Qin = 2 # [l³/t] 
 Qout = 0.6 #[l³/t]  Assuming 70% ET
+#Number of compartments
+numc = 6
 #These would be from an input file for each chemical.
 
 chems = ['c1', 'c2', 'c3', 'c4', 'c5']
@@ -70,15 +74,15 @@ DT6 = 0.1
 Z1 = 0.1 #[M/L³]
 
 #inputs
-inp_1 = 1
-inp_2 = 2
-inp_3 = 3
-inp_4 = 4
-inp_5 = 5
-inp_6 = 6
+inp_1 = 0
+inp_2 = 0
+inp_3 = 0
+inp_4 = 0
+inp_5 = 0
+inp_6 = 0
 #Initialize length variable
-numc = len(chems)
-res = pd.DataFrame(np.arange(0,L+dx,dx),columns = ['x'])
+numchems = int(len(chems))
+res = pd.DataFrame(np.arange(0,L+dx-1,dx),columns = ['x'])
 #Define the x term as the centre of each cell
 res.loc[:,'x'] = res.x+dx/2
 res1 = res
@@ -86,8 +90,8 @@ res2 = res
 res3 = res
 res4 = res
 res = pd.concat([res,res1,res2,res3,res4], keys=chems)
-numx = len(res.x)/numc
-reslen = len(res.x)
+numx = int(len(res.x)/numchems)
+reslen = int(len(res.x))
 #Calculate the flow for every point in x
 res.loc[:,'V1'] = A*dx #water volume of each x [L³]
 res.loc[:,'dx'] = dx #distance between nodes [L]
@@ -132,6 +136,7 @@ res.loc[:,'D_63'] = D63
 res.loc[:,'D_64'] = D64
 res.loc[:,'D_65'] = D65
 res.loc[:,'DT6'] = DT6
+res.loc[:,'Z1'] = Z1
 res.loc[:,'inp_1'] = inp_1
 res.loc[:,'inp_2'] = inp_2
 res.loc[:,'inp_3'] = inp_3
@@ -140,27 +145,37 @@ res.loc[:,'inp_5'] = inp_5
 res.loc[:,'inp_6'] = inp_6
 
 #Conditions in prior time step (initial conditions for each step)
-res.loc[:,'aw_t'] = 11 - (res.x - numx)/numx
+res.loc[:,'aw_t'] = 1 - (res.x - numx)/numx
 res.loc[:,'as_t'] = 0 #initial activity in the soil
 #Boundary Conditions - Type 1 upstream
-bc_us = 1 #Activity at the source
+bc_us = 0.05 #Activity at the source
 #Type 2 boundary downstream
 bc_ds = 0
 
 """
 From here, everything should be general and applicable in the class method
 """
-#Calculate forward and backward facial V, q and disp
-#Back and forward facial volumes (L³)
+#Calculate forward and backward facial values
+#Back and forward facial Volumes (L³)
 res.loc[1:reslen,'V1_b'] = (res.V1.shift(1) + res.V1)/2
 res.loc[(slice(None), 0),'V1_b'] = res.loc[(slice(None),0),'V1']
 res.loc[0:reslen-1,'V1_f'] = (res.V1.shift(-1) + res.V1)/2
 res.loc[(slice(None), numx-1),'V1_f'] = res.loc[(slice(None),numx-1),'V1']
-#Darcy's flux (L/T)
+#Darcy's flux, q, (L/T)
 res.loc[1:reslen,'q_b'] = (res.q.shift(1) + res.q)/2
-res.loc[(slice(None), 0),'q_b'] = res.loc[(slice(None),0),'q']
+res.loc[(slice(None), 0),'q_b'] = Qin/A
 res.loc[0:reslen-1,'q_f'] = (res.q.shift(-1) + res.q)/2
-res.loc[(slice(None), numx-1),'q_f'] = res.loc[(slice(None), numx-1),'q']
+res.loc[(slice(None), numx-1),'q_f'] = Qout/A
+#Dispersivity disp [l²/T]
+res.loc[1:reslen,'disp_b'] = (res.disp.shift(1) + res.disp)/2
+res.loc[(slice(None), 0),'disp_b'] = res.loc[(slice(None), 0),'disp']
+res.loc[0:reslen-1,'disp_f'] = (res.disp.shift(-1) + res.disp)/2
+res.loc[(slice(None), numx-1),'disp_f'] = res.loc[(slice(None), numx-1),'disp']
+#Activity/Fugacity capacity Z
+res.loc[1:reslen,'Z1_b'] = (res.Z1.shift(1) + res.Z1)/2
+res.loc[(slice(None), 0),'Z1_b'] = res.loc[(slice(None),0),'Z1']
+res.loc[0:reslen-1,'Z1_f'] = (res.Z1.shift(-1) + res.Z1)/2
+res.loc[(slice(None), numx-1),'Z1_f'] = res.loc[(slice(None),numx-1),'Z1']
 
 #DISCUS algortithm semi-lagrangian 1D ADRE from Manson & Wallis (2000) DOI: 10.1016/S0043-1354(00)00131-7
 #Outside of the time loop, if flow is steady, or inside if flow changes
@@ -190,7 +205,6 @@ xf_test1 = xb_test1.copy(deep = True)
 #This loop calculates the distance & time backwards that a water packet takes
 #in a time step. 
 dels = 0
-
 for dels in range(int(max(np.floor(res.c)))): #Max cells any go through (could be wrong if q increases)
     #Time to traverse a full cell
     delb_test += res.groupby(level = 0)['del_0'].shift(dels+1)
@@ -215,152 +229,132 @@ delrb_test[delrb_test==0] = dt - delb_test1
 delrf_test[delrf_test==0] = dt - delf_test1
 xb_test1[np.isnan(xb_test1)] = xb_test + delrb_test * res.groupby(level = 0)['q'].shift(dels+1)
 xf_test1[np.isnan(xf_test1)] = xf_test + delrf_test * res.groupby(level = 0)['q'].shift(dels)        
-#And we can deal with those boundary conditions. Everything at the US boundary
-#starts before the origin, so we will just define it as zero.
-#xb_test1[np.isnan(xb_test1)] = 0
-#xf_test1[np.isnan(xf_test1)]  = 0
 #Bring what we need to res. The above could be made a function to clean things up too.
 #Distance from the origin of the forward and back faces
-res.loc[:,'xb'] = res.x - xb_test1
-res.loc[:,'xf'] = res.x + res.dx - xf_test1
-#Clean up the US boundary, anything NAN or zero comes from before the origin
-res.loc[0:dels+1,'xb'] = 0
-res.loc[0:dels,'xf'] = 0
-#Now we define the cumulative mass along the entire length of the system as M(x) = sum (Mi)
+res.loc[:,'xb'] = res.x - res.dx/2 - xb_test1
+res.loc[:,'xf'] = res.x + res.dx/2 - xf_test1
+#Clean up the US boundary, anything NAN or < 0 comes from before the origin
+maskb = (np.isnan(res.xb) | (res.xb < 0))
+maskf = (np.isnan(res.xf)) | (res.xf < 0)
+res.loc[maskb,'xb'] = 0
+res.loc[maskf,'xf'] = 0
+#Define the cumulative mass along the entire length of the system as M(x) = sum (Mi)
+#This is defined at the right hand face of each cell. M = ai*Zi*Vi, at time n
+res.loc[:,'M_i'] = res.aw_t * res.Z1 * res.V1
+res.loc[:,'M_n'] = res.groupby(level = 0)['M_i'].cumsum()
+#Then, we advect one time step. To advect, just shift everything as calculated above.
+#We will use a cubic interpolation. Unfortunately, we have to unpack the data 
+#in order to get this to work.
+chems = res.index.levels[0]
+for ii in range(numchems):
+    xx = res.loc[(chems[ii], slice(None)),'x']
+    yy = res.loc[(chems[ii], slice(None)),'M_n']
+    f = interp1d(xx,yy,kind='cubic',bounds_error = False)
+    res.loc[(chems[ii], slice(None)),'M_star'] = f(res.loc[(chems[ii], slice(None)),'xf'])\
+    - f(res.loc[(chems[ii], slice(None)),'xb'])
+    #US boundary conditions
+#Case 1 - both xb and xf are outside the domain
+mask = (res.xb == 0) & (res.xf == 0)
+res.loc[mask,'M_star'] = Qin*bc_us*dt
+#Case 2 - xb is out of the range, but xf is in
+#Need to compute the sum of a spatial integral from x = 0 to xf and then the rest is temporal
+mask = (res.xb == 0) & (res.xf != 0)
+slope = np.array(res.M_n[(slice(None),0)])/np.array((res.dx[(slice(None),0)]))
+M_x = slope*np.array(res.xf[mask])
+#Time integration
+M_t =  (dt-np.array(res.xf[mask]/res.q[mask]))*Qin*bc_us
+res.loc[mask,'M_star'] = M_x + M_t
+#Case 3 - too close to origin for cubic interpolation, so we will use linear interpolation
+mask = np.isnan(res.M_star)
+res.loc[mask,'M_star'] = slope * (res.xf[mask] - res.xb[mask])
+#Divide out to get back to activity/fugacity entering from advection
+res.loc[:,'a_star'] = res.M_star / res.Z1 / res.V1
 
-
-
-#Distance in x that the CV faces move through in time step dt
-res.loc[:,'xb'] = 0
-res.loc[:,'xf'] = 0
-#Time past last cell back
-res.loc[:,'del_rb'] = 0
-res.loc[:,'del_rf'] = 0
-
-res.loc[:,'del_rb'] = dt - del_test1
-    res.loc[:,'del_tf'] += res.groupby(level = 0)['del_0b'].shift(dels+1)
-    delrb_test = dt - del_t
-    res.loc[:,'del_rb'] += dt -  res.groupby(level = 0)['del_0b'].shift(dels+1)
-    res.loc[:,'xb'] += res.groupby(level = 0)['del_0b'].shift(dels+1)
-
-    
-res.loc[:,'c_trunc'] = np.floor(res.c)
-#Remnant courant number c_alpha. This is always <1
-res.loc[:,'c_alpha'] = res.c - res.c_trunc
-#Now, we need to determine the back and forward faces for each CV
-#For this, we can calculate the travel time across each previous cell as:
-d = {}
-for dels in range(int(max(np.floor(res.c)-1))): #Pass fully through floor(c)-1 cells
-    d["del_{0}".format(dels+1)] = res.groupby(level = 0)['q'].shift(dels+1)\
-    /res.groupby(level = 0)['dx'].shift(dels+1)
-res = pd.concat([res,pd.DataFrame(d)],axis = 1, sort = False)
-res.loc[:,'del_t']
-
-#Dispersivity [l²/T]
-res.loc[1:reslen,'disp_b'] = (res.disp.shift(1) + res.disp)/2
-res.loc[(slice(None), 0),'disp_b'] = res.loc[(slice(None), 0),'disp']
-res.loc[0:reslen-1,'disp_f'] = (res.disp.shift(-1) + res.disp)/2
-res.loc[(slice(None), numx-1),'disp_f'] = res.loc[(slice(None), numx-1),'disp']
-
-#Start solving the QUICKEST algorithm for time t+1, from time t. 
-#First, solve for the aw_f (front) for each x
-#Internal CVs
-res.loc[1:reslen,'aw_f'] = 0.5*(res.aw_t.shift(-1)+res.aw_t) - res.c/2*((res.aw_t.shift(-1)-res.aw_t)\
-       - 1/6 *((1-res.c**2)*((res.aw_t.shift(-1)-2*res.aw_t+res.aw_t.shift(1)))))
-#Upstream boundary
-res.loc[(slice(None), 0),'aw_f'] = np.array(0.5*(res.aw_t[slice(None),1]+res.aw_t[slice(None),0])\
- - res.c[0]/2*((res.aw_t[slice(None),1]-res.aw_t[slice(None),0])-1/6 \
- *((1-res.c[slice(None),0]**2)*((res.aw_t[slice(None),1]-2*res.aw_t[slice(None),0]+bc_us)))))
-#Downstream boundary
-res.loc[(slice(None),numx-1),'aw_f'] = np.array(res.aw_t[slice(None),numx-1]-res.c[slice(None),numx-1]/2\
- *((1/6)*(1-res.c[slice(None),numx-1]**2)*(res.aw_t[slice(None),numx-1]-2\
-    *res.aw_t[slice(None),numx-1]+res.aw_t[slice(None),numx-2])))
-#Next, solve fore aw_b (back) for each x
-#Internal CVs - no need for DS calculation
-res.loc[2:reslen,'aw_b'] = 0.5*(res.aw_t+res.aw_t.shift(1)) - res.c/2*((res.aw_t-res.aw_t.shift(1))\
-       - 1/6 *((1-res.c**2)*((res.aw_t-2*res.aw_t.shift(1)+res.aw_t.shift(2)))))
-#Upstream boundary
-res.loc[(slice(None),0),'aw_b'] = np.array(0.5*(res.aw_t[slice(None),0]+bc_us) - res.c[slice(None),0]\
-/2*((res.aw_t[slice(None),0]-bc_us) - 1/6 *((1-res.c[slice(None),0]**2)*\
-       ((res.aw_t[slice(None),0]-2*bc_us+bc_us)))))
-#Cell 1, need to solve as  this one goes 2 cells back
-res.loc[1,'aw_b'] = 0.5*(res.aw_t[1]+res.aw_t[0]) - res.c[1]/2*((res.aw_t[1]-res.aw_t[0])\
-       - 1/6 *((1-res.c[0]**2)*((res.aw_t[1]-2*res.aw_t[0]+bc_us))))
-
-
-#Then we can solve for (aZV)*
-#Time and space weighting term, P
-res.loc[0,'P'] =dt/(res.dx[0])
-res.loc[1:numx-1,'P'] = 2*dt/(res.dx.shift(1) + res.dx)
-#Interior cells
-res.loc[0:numx-1,'aZV*'] = res.aw_t*Z1*res.V1 + res.P*(res.aw_f*Z1*res.V1_f*res.q_f\
-    -res.aw_b*Z1*res.V1_b*res.q_b)
 #Finally, we can now set up & solve our implicit portion. 
-#Let's define the wpacial weighting terms as f, m, b. b will be
-#for the (i-1) spacial step, m for (i), f for (i+1)
-#the back (b) term
-res.loc[0,'b'] = res.P[0]*res.V1_b[0]*Z1*res.disp_b[0]/res.dx[0]
-res.loc[1:numx-1,'b'] = res.P*res.V1_b*Z1*res.disp_b/res.dx.shift(1)
-#forward (f) term
-res.loc[0:numx-1,'f'] = res.P*res.V1_f*Z1*res.disp_f/res.dx
-#Middle (m) term
-res.loc[0:numx-1,'m'] = res.P*(-res.f-res.b) - dt*res.DT1
-#Upstream boundary condition m_star
-m_star = res.m[0]
-#downstream boundary condition m_etoile
-#Currently not sure if the bc_ds works other than as an on/off switch
-m_etoile = res.P[numx-1]*(-res.f[numx-1]*bc_ds-res.b[numx-1]) - dt*res.DT1[numx-1]
+#We are using a method based on those of Manson and Wallis (1995) and Kilic & Aral (2009)
+#Define the spatial weighting term (P) 
+res.loc[:,'P'] =dt/(res.dx)
+#Now define the spacial weighting terms as f, m, & b. 
+#b for the (i-1) spacial step, m for (i), f for (i+1)
+#the back (b) term acting on x(i-1)
+res.loc[:,'b'] = 2*res.P*res.V1_b*res.Z1_b*res.disp_b/(res.dx + res.groupby(level = 0)['dx'].shift(1))
+#To deal with the upstream boundary condition, we can simply set dx(i-1) = dx so that:
+res.loc[(slice(None),0),'b'] = 2*res.P*res.V1_b*res.Z1_b*res.disp_b/(res.dx)
+#forward (f) term acting on x(i+1)
+res.loc[:,'f'] = 2*res.P*res.V1_f*res.Z1_f*res.disp_f/(res.dx + res.groupby(level = 0)['dx'].shift(-1))
+res.loc[(slice(None),numx-1),'f'] = 0
+#Middle (m) term acting on x(i) - this will be subracted in the matrix (-m*ai)
+#Upstream and downstream BCs have been dealt with in the b and f terms
+res.loc[:,'m'] = res.f+res.b+dt*res.V1*res.Z1*res.DT1
 #These will make the matrix. For each spacial step, i, there will be
 #numc activities that we will track. So, in a system of water, air and sediment
 #you would have aw1, as1, aa1, aw2,as2,aa3...awnumc,asnumc,aanumc, leading to a matrix
 #of numc * i x numc * i in dimension.
 #Initialize transport matrix and RHS vector (inp)
-mat = np.zeros([numx*numc,numx*numc])
-inp = np.zeros([numx*numc,1])
-#mat = np.array(mat)
-#FILL THE MATRIX
+mat = np.zeros([numx*numc,numx*numc,numchems])
+inp = np.zeros([numx*numc,numchems])
+#FILL THE MATRICES
+i,j,k = [0,0,0]
+#First, define where the matrix values will go.
+m_vals = np.arange(0,numx*numc,numc)
+b_vals = np.arange(numc,numx*numc,numc)
+start = time.time()
+#Then, we can set the ADRE terms. Since there will always be three no need for a loop.
+mat[m_vals,m_vals,:] = -np.array(res.m).reshape(numchems,numx).swapaxes(0,1)
+mat[b_vals,m_vals[0:numx-1],:] = np.array(res.loc[(slice(None),slice(1,numx)),'b']).reshape(numchems,numx-1).swapaxes(0,1)
+mat[m_vals[0:numx-1],b_vals,:] = np.array(res.loc[(slice(None),slice(0,numx-2)),'f']).reshape(numchems,numx-1).swapaxes(0,1)
+#Next, set D values and inp values
+for j in range(0,numc): #j is the row index
+    for k in range(0,numc): #k is the column index
+        if (j == k): 
+            if j == 1:#Skip DT1 as it is in the m value
+                pass
+            else: #Otherwise, place the DT values in the matrix
+                D_val = 'DT' + str(k+1)
+                mat[m_vals+j,m_vals+j,:] = -np.array(res.loc[:,D_val]).reshape(numchems,numx).swapaxes(0,1)
+        else: #Place the intercompartmental D values
+            D_val = 'D_' +str(k+1)+str(j+1)
+            mat[m_vals+j,m_vals+k,:] = -np.array(res.loc[:,D_val]).reshape(numchems,numx).swapaxes(0,1)
+#Upstream boundary - need to add the diffusion term. DS boundary is dealt with already
+inp[0,:] += -res.b[slice(None),0]*bc_us
+end = time.time()
+print(end - start)
+start = time.time()
+#maskb = mat[numx/numc] +
 for i in range(0,numx-1):
-    if (i-1)*numc >= 0:
-        mat[i*numc,(i-1)*numc] = res.b[i]
-    mat[i*numc,i*numc] = res.m[i]
-    mat[i*numc,(i+1)*numc] = res.f[i]
-    inp[i*numc] = -res.inp_1[i]
+    if i == 588:
+        x = 1
+    if (i-1)*numc >= 0: #No b term at US boundary
+        mat[i*numc,(i-1)*numc,:] = res.b[slice(None),i]
+    mat[i*numc,i*numc,:] = -res.m[slice(None),i] #M BCs already dealt with
+    if (i+1)*numc <=(numx-1): #No f term at DS boundary
+        mat[i*numc,(i+1)*numc,:] = res.f[slice(None),i]
+    inp[i*numc,:] = res.M_i[slice(None),i] - res.M_star[slice(None),i] - res.inp_1[slice(None),i]
     for j in range(2,numc+1):
         D_val = 'D_' +str(j)+str(1)
-        mat[i*numc,i*numc+j-1] = res.loc[i,D_val]
+        mat[i*numc,i*numc+j-1,:] = res.loc[(slice(None),i),D_val]
         inp_val = 'inp_' +str(j)
-        inp[i*numc+j-1] = -res.loc[i,inp_val]
+        inp[i*numc+j-1,:] = -res.loc[(slice(None),i),inp_val] #Inter-compartment transfer just the regular inputs
         for k in range(1,numc+1):
             if j == k:
                 D_val = 'DT' +str(k)
-                mat[i*numc+j-1,i*numc+k-1] = -1*res.loc[i,D_val]
+                mat[i*numc+j-1,i*numc+k-1,:] = -1*res.loc[(slice(None),i),D_val]
             else:
                 D_val = 'D_' +str(k)+str(j)
-                mat[i*numc+j-1,i*numc+k-1] = res.loc[i,D_val]
-#Upstream boundary
-mat[0,0] = m_star
-inp[0,0] = -res.loc[0,'inp_1'] - res.f[0]*bc_us
-#for i in range(2,numc+1):
-#    D_val = 'D_' +str(i)+str(1)
-#    mat[0,i-1] = res.loc[i,D_val]
-#Downstream Boundary - have to subtract numc as compartments are in line
-mat[numx*numc-numc,numx*numc-2*numc] = res.b[i]
-mat[numx*numc-numc,numx*numc-numc] = m_etoile
-inp[numx*numc-numc] = -res.loc[numx-numc,'inp_1']
-for j in range(2,numc+1):
-    D_val = 'D_' +str(j)+str(1)
-    mat[numx*numc-numc,numx*numc-numc+j-1] = res.loc[i,D_val]
-    inp_val = 'inp_' +str(j)
-    inp[numx*numc-numc+j-1] = -res.loc[numx-numc+j-1,inp_val]
-    for k in range(1,numc+1):
-        if j == k:
-            D_val = 'DT' +str(k)
-            mat[numx*numc-numc+j-1,numx*numc-numc+k-1] = -1*res.loc[i,D_val]
-        else:
-            D_val = 'D_' +str(k)+str(j)
-            #I should probably simplify this but ¯\_(ツ)_/¯
-            mat[numx*numc-numc+j-1,numx*numc-numc+k-1] = res.loc[i,D_val]
-matsol = np.linalg.solve(mat,inp)
+                mat[i*numc+j-1,i*numc+k-1,:] = res.loc[(slice(None),i),D_val]
+#Upstream boundary - need to add the diffusion term. DS boundary is dealt with in the loop
+inp[0,:] += -res.b[slice(None),0]*bc_us
+end = time.time()
+print(end - start)
+#Now, we will solve the matrix for each compound in turn (might be possible to do simultaneously)
+for ii in range(numchems):
+    chem = chems[ii]
+    LHS = mat[:,:,ii]
+    RHS = inp[:,ii]
+    matsol = np.linalg.lstsq(LHS,RHS)
+    matsol = np.linalg.solve(mat[:,:,ii],inp[:,ii])
+    #Put in to the results array
+    res.loc[(chem,slice(None)),'a_n1'] = matsol
 #res.loc[:,'matsol'] = matsol
 end = time.time()
 print(end - start)
